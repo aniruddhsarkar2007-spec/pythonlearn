a5091e4eee70d271333e8fc9f2ca5bac48b2dee6
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-04-06 13:35:00 +0530

    Learning py

---
+++ list.py
@@ -0,0 +1,84 @@
+# l=eval(input("Enter any list:"))
+# print(l)
+# print(type(l))
+# print(id(l))
+# print(len(l))
+# print(max(l))
+# print(min(l))
+#print(sum(l)) 
+
+# There are no functions in list it has methods
+
+
+#l=[1,3,4,7,8,9,5]
+#l1=l.copy()
+#print(l1,l)
+#print(id(l),id(l1))
+
+#l.append(10)
+#print(l)
+
+
+#l=eval(input("Enter any list:"))
+#element=eval(input("Enter element:"))
+#l.append(element)
+#print(l)
+
+# l=eval(input("Enter any list:"))
+# element=eval(input("Enter element:"))
+# l.extend(element)
+# print(l)
+
+# l.extend(23,5)
+# print(l)
+
+
+# l=eval(input("Enter any list:"))
+# element=eval(input("Enter element:"))
+# position=int(input("Enter index value:"))
+# l.insert(position,element)
+# print(l)
+
+
+# l=[1,2,3,4,5,6]
+# l.pop()
+# print(l)
+# l.pop(1)
+# print(l)
+
+
+# l=[1,2,3,4,5,6]
+# l.remove(3)
+# print(l)
+
+
+l=[1,2,3,4,5,6]
+l.sort(reverse=True)
+print(l)
+
+#l.pop(3)
+#print(l)
+
+#l.sort()
+#print(l)
+
+#l.reverse()
+#print(l)
+
+
+
+
+
+
+
+#copy()
+# clear()
+# append()
+# extend()
+# insert()
+# pop()
+# remove()
+# sort()
+# reverse()
+# coutn()
+# index()
--- strmethodsD2.py
+++ strmethodsD2.py
@@ -8,15 +8,43 @@
 # print(s.endswith('n'))
 # print(s.capitalize())
 # print(s.index("h"))
+# print(s.spllit())
+# print(s.join())
+
 
 
 # DYNAMIC COUNT
 
-s=input("Enter any string:")
-ch=input("Enter any character:")
-print(s.count(ch))
+# s=input("Enter any string:")
+# ch=input("Enter any character:")
+# print(s.count(ch))
  
 
 # start=int(input("Enter start point:"))
 # stop=int(input("Enter stop point:"))
 # print(s.index(input("Enter any character:"),int(input("Enter start point:"),int(input("Enter stop point:")))))
+
+
+# SYNTAX OF SPLIT
+# string.split(From where to break,How many times to break)
+
+# s="this is python"
+# print(s.split())
+# print(s.split('p'))
+# print(s.split(' ',2))
+# print(s.split(' ',0))
+
+#-----------JOIN--------------
+#ITERABLE MEANS COLLECTION
+
+# s1="PYTHON"  
+# s2="JAVA"
+# s3="PHP"   
+# x=','.join([s1,s2,s3])
+# print(x)
+
+
+s="!!!Python!!!"
+print(s.strip("!"))
+print(s.lstrip("!"))
+print(s.rstrip("!"))
