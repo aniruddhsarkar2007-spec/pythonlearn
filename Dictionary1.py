fac7b06d967e2c1e1c4e1dcea473aec4de2d4d32
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-04-09 13:26:01 +0530

    Learning python

---
+++ Dictionary1.py
@@ -0,0 +1,67 @@
+# DICTIONARY
+
+# COLLECTION OF KEY VALUE PAIRS
+# INCLOSED WITH {}
+# PAIRS ARE SEPERATED BY COMMA
+# KEY VALUE SEPERATED BY COLON(:)
+# * KEY MUST BE UNIQUE VALUE MAY BE DUPLICATE
+# * INDEXING NOT SUPPORTED
+# * SLICING NOT SUPPORTED
+#  MUTTABLE IN NATURE
+
+
+# d={"Name":"Aniruddh",
+#     "Age":18,
+#     "Qual":"BCA"    
+#     }
+# print(d)
+# print(type(d))
+# print(max(d))
+# print(min(d))
+
+
+# d={1:"Aniruddh",
+#     2:18,
+#     '3':"BCA"    
+#     }
+# print(d)
+# print(sum(d))
+
+
+# METHIDS OF DICTIONARY
+# COPY() - CREATE NEW OBJECT
+# CLEAR() - CLEAR KEY AND VALUES MAKE AN EMPTY DICTIONARY
+# GET() - d.get('key) TO GET THE VALUE
+# KEYS() - RETURNS THE KEYS
+# VALUES() - RETURNS THE VALUES
+# ITEMS() - RETURN THE PAIR OF KEY AND VALUE 
+# FROMKEYS() - 
+# UPDATE()
+# SETDEFAULT()
+
+
+# ITERABLE - COLECTION OF ELEMENTS
+
+# d=eval(input("Enter any dict"))
+# d.pop('name')
+# d.popitem()
+# print(d)
+# d.popitem()
+# print(d)
+
+
+# l=["python","java","c++"]
+# s="python"
+# d=dict().fromkeys(s,"10")
+# print(d)
+
+# l={"name":"Aniruddh","age":18}
+# h={"class":"BCA","city":"Bhopal"}
+# l.update(h)
+# print(l)
+
+
+# d={"name":"A","class":"bca"}
+# d.setdefault("age",17)
+# print(d)
+
