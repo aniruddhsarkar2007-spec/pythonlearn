7655898f6822eba42305a9d314b78409aaccfe68
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-04-10 13:29:47 +0530

    Learning python

---
+++ set1.py
@@ -0,0 +1,43 @@
+# COLLECTION OF UNIQUE ELEMENTS
+# ENCLOSED WITH {} AND COMMA(,) SEPERATED ELEMENTS
+# UNORDERED COLLECTION
+# INDEXING NOT SUPPORTED
+# SLICING NOT SUPPORTED 
+# MUTABLE IN NATURE
+
+s={10,20,2,"Python","Java"}
+print(s)
+print(type(s))
+print(id(s))
+print(len(s))
+# print(max(s))
+# print(min(s))
+
+
+# METHOD FOR 2 OR MORE SET REQUIREMENT
+# UNION()               ---------
+# INTERSECTION()                |   NEW SET CREATED
+# DIFFERENCE()                  |
+# SYMETRIX DIFFERENCE() ---------
+
+
+# INTERSECTION UPDATE()           -------
+# DIFFERENCE UPDATE()                    | 
+# SYMETRIC DIFFERENCE UPDATE()    -------
+
+
+# ISSUBSET()
+# ISSUPRESET
+
+
+s1={1,2,3,4,5}
+s2={4,5,6,7,8}
+print(s1.union(s2))
+print(s1.intersection(s2))
+print(s1.difference(s2))
+print(s1.symmetric_difference(s2))
+ 
+print(s1.intersection_update(s2))
+
+print(s1)
+
