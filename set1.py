dbe2319d0b96ccbc5f3e048e818c886c17874167
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-04-14 13:32:37 +0530

    Learning python

--- set1.py
+++ set1.py
@@ -5,11 +5,11 @@
 # SLICING NOT SUPPORTED 
 # MUTABLE IN NATURE
 
-s={10,20,2,"Python","Java"}
-print(s)
-print(type(s))
-print(id(s))
-print(len(s))
+# s={10,20,2,"Python","Java"}
+# print(s)
+# print(type(s))
+# print(id(s))
+# print(len(s))
 # print(max(s))
 # print(min(s))
 
@@ -30,14 +30,31 @@ print(len(s))
 # ISSUPRESET
 
 
-s1={1,2,3,4,5}
-s2={4,5,6,7,8}
-print(s1.union(s2))
-print(s1.intersection(s2))
-print(s1.difference(s2))
-print(s1.symmetric_difference(s2))
+# s1={1,2,3,4,5}
+# s2={4,5,6,7,8}
+# print(s1.union(s2))
+# print(s1.intersection(s2))
+# print(s1.difference(s2))
+# print(s1.symmetric_difference(s2))
  
-print(s1.intersection_update(s2))
+# print(s1.intersection_update(s2))
 
-print(s1)
+# print(s1)
 
+# METHODS
+
+# ADD() - ADDS SIGNLE ELEMENT IN RANDOM POSITION
+# UPDATE() - ADD MULTIPLE ELEMENTS IN RANDOM POSITION
+
+
+# REMOVE() - REMOVE ELEMENT
+# DISCARD()
+
+# POP() - REMOVE RANDOM ELEMENT
+# CLEAR() - REMOVE ALL THE ELEMENTS
+# COPY() - MAKE COPY
+
+# s={10,20,"python","java"}
+# ne="python124"
+# s.add(ne)
+# print(s)
