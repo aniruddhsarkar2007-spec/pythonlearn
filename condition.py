7ea5e39abc4694da31a5db5bfc668501810beeab
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-04-16 13:30:01 +0530

    Learning python

---
+++ condition.py
@@ -0,0 +1,7 @@
+n=int(input("Enter any no:"))
+if(n%2==0):
+    print(f"given no {n} is even")
+elif(n%2!=0):
+    print(f"given no {n} is odd" )
+elif(n==0):
+    print(f"given no {n} is zero")    
