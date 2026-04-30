c6ce796635e10c927d060242d54ad8813d8e032a
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-04-30 22:11:27 +0530

    Learning Python

--- myprc1.py
+++ myprc1.py
@@ -6,3 +6,9 @@ for i in range (1,n+1):
 l=eval(input("Enter any tuple:"))
 print(l.index(2))
 print(l.count(3))      
+
+
+s="!!!Python!!!"
+print(s.strip("!"))
+print(s.lstrip("!"))
+print(s.rstrip("!"))
