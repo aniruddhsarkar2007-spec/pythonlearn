a92963fdb7128a7c8c974a4c974317a98625fa25
Aniruddh sarkar <aniruddhsarkar2007@gmail.com>
2026-05-05 13:27:34 +0530

    Learning Python

---
+++ function.py
@@ -0,0 +1,24 @@
+# FUNCTION
+# write ones call multiple time
+# code reusable
+
+# Syntax:
+#   --------------
+#     Decleration
+#   --------------  
+#     Calling
+#   -------------- 
+
+
+# required: def,function_name,(),:
+
+# optional: parameter,argument,return (for termenating the function)    
+ 
+ 
+def add():
+    print("Addition is:",5+6)
+    return
+add()
+print(add())
+
+#pp8
---
+++ new.py
@@ -0,0 +1,11 @@
+# x=int(input("Enter any no"))
+# y=int(input("Enter any no"))
+# if(x>y):
+#     print("X is greater")
+# else:
+#     print("Y is greater than X")
+
+
+x=(input("Enter your name:"))
+x=(input("Enter your age:"))
+print(x)
